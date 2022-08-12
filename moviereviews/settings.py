578186373804f45479c1036c785cc0e595b7919a
András Novoszáth <nocibambi@gmail.com>
2022-08-12 15:46:13 +0200

    remove security key + debug

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)ln=7+ocnw_j@jrpsk(3+3d7ahj7le_+$xhheqq4bynd3)+mgl"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
